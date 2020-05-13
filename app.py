bb23d0d419b00d2ec66c4cf786f53ebf056f0588
eoinahern <eoin.ahern@mycit.ie>
2020-05-13 20:43:56 +0100

    minor code cleanup

--- app.py
+++ app.py
@@ -11,13 +11,19 @@ def index():
         return render_template("main_page.html")
     elif request.method == "POST":
         url = request.form.get("url")
+        return extract_video_data(url)
 
-        if youtube_url_partial in url:
-            video = YouTube(url)
-            vid_streams = video.streams.filter(mime_type="video/mp4", progressive=True)
-            video_title = video.title
-            dictionary = {i: vid_streams[i] for i in range(0, len(vid_streams))}
-            return render_template("search_complete.html", streams=dictionary, title=video_title)
-        else:
-            return render_template("error_loading.html")
 
+def extract_video_data(url):
+    if youtube_url_partial in url:
+        video = YouTube(url)
+        vid_streams = video.streams.filter(mime_type="video/mp4", progressive=True)
+        video_title = video.title
+        dictionary = {i: vid_streams[i] for i in range(0, len(vid_streams))}
+        return render_template("search_complete.html", streams=dictionary, title=video_title)
+    else:
+        return render_template("error_loading.html")
+
+
+if __name__ == "__main__":
+    app.run()
